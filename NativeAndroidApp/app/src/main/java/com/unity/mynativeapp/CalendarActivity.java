2f14463ade9b6468ca2a13d63a79f1b41f630a84
lee <rbsghks01@gmail.com>
2020-05-21 18:20:27 +0900

    calendar->datePicker add method

--- NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/CalendarActivity.java
+++ NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/CalendarActivity.java
@@ -4,11 +4,40 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
+    DatePicker calendar;
+    TimePicker startTime=findViewById(R.id.time1);
+    TimePicker endTime=findViewById(R.id.time2);
+    String selectedDate;
+    String selectStart;
+    String selectEnd;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_list);
+        calendar= (DatePicker) findViewById(R.id.calendar);
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedDate=calendar.getYear()+"/"+calendar.getMonth()+"/"+calendar.getDayOfMonth();
+            }
+        });
+//        startTime.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SimpleDateFormat sdf=new SimpleDateFormat("HH:MM");
+//                selectStart=startTime.getCurrentHour()+":"+startTime.getCurrentMinute();
+//                Log.d("time:",selectStart);
+//            }
+//        });
     }
 }
